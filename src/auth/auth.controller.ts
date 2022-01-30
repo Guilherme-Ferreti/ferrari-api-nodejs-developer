41f14d8132923369867c4cb1a8d0bbab55d2706d
Guilherme-Ferreti <guiferretis@gmail.com>
2022-01-30 15:00:29 -0300

    Register and update users

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,46 @@
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { parse } from 'date-fns';
+import { UserService } from 'src/user/user.service';
+
+
+@Controller('auth')
+export class AuthController {
+  constructor(private userService: UserService) {}
+  
+  @Post()
+  async verifyEmail(@Body('email') email: string) {
+    try {
+      await this.userService.getByEmail(email);
+      return { exists: true };
+    } catch (e) {
+      return { exists: false };
+    }
+  }
+
+  @Post('register')
+  async register(
+    @Body('name') name,
+    @Body('email') email,
+    @Body('birthAt') birthAt,
+    @Body('phone') phone,
+    @Body('document') document,
+    @Body('password') password,
+  ) {
+    if (birthAt) {
+      try {
+        birthAt = parse(birthAt, 'yyyy-MM-dd', new Date());
+      } catch (e) {
+        throw new BadRequestException('BirthAt is invalid!');
+      }
+    }
+
+    return this.userService.create({
+      name,
+      email,
+      birthAt,
+      phone,
+      document,
+      password
+    });
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,7 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
@@ -25,6 +27,8 @@ export class UserService {
       throw new NotFoundException('User not found!');
     }
 
+    delete user.password;
+
     return user;
   }
 
@@ -46,6 +50,139 @@ export class UserService {
       throw new NotFoundException('User not found!');
     }
 
+    delete user.password;
+
     return user;
+  }
+
+  async create({ 
+    name,
+    email,
+    birthAt,
+    phone,
+    document,
+    password,
+  }:{
+    name: string;
+    email: string;
+    password: string,
+    birthAt?: Date;
+    phone?: string;
+    document?: string;
+  }) {
+    if (! name) {
+      throw new BadRequestException('Name is required!');
+    }
+
+    if (! email) {
+      throw new BadRequestException('E-mail is required!');
+    }
+
+    if (! password) {
+      throw new BadRequestException('Password is required!');
+    }
+
+    if (birthAt && birthAt.toString().toLowerCase() == 'invalid date') {
+      throw new BadRequestException('BirthAt is invalid!');
+    }
+
+    let user = null;
+
+    try {
+      user = await this.getByEmail(email);
+    } catch (e) {
+      
+    }
+
+    if (user) {
+      throw new BadRequestException('E-mail already exists!');
+    }
+
+    const createdUser = await this.prisma.user.create({
+      data: {
+        person: {
+          create: {
+            name,
+            birthAt,
+            document,
+            phone,
+          },
+        },
+        email,
+        password: bcrypt.hashSync(password, 10),
+      },
+      include: {
+        person: true,
+      },
+    });
+
+    delete createdUser.password;
+
+    return createdUser;
+  }
+
+  async update(id: number, { 
+    name,
+    email,
+    birthAt,
+    phone,
+    document,
+  }:{
+    name?: string;
+    email?: string;
+    birthAt?: Date;
+    phone?: string;
+    document?: string;
+  }) {
+    id = Number(id);
+
+    if (isNaN(id)) {
+      throw new BadRequestException('ID is not a number!');
+    }
+
+    const personData = {} as Prisma.PersonUpdateInput;
+    const userData = {} as Prisma.UserUpdateInput;
+
+    if (name) {
+      personData.name = name;
+    }
+    
+    if (birthAt) {
+      personData.birthAt = birthAt;
+    }
+    
+    if (phone) {
+      personData.phone = phone;
+    }
+
+    if (document) {
+      personData.document = document;
+    }
+
+    if (email) {
+      userData.email = email;
+    }
+
+    const user = await this.get(id);
+
+    if (personData) {
+      await this.prisma.person.update({
+        where: {
+          id: user.personId,
+        },
+        data: personData
+      });
+    }
+
+    if (userData) {
+      await this.prisma.user.update({
+        where: {
+          id,
+        },
+        data: userData
+      });
+    }
+
+    return this.get(id);
   }
 }
