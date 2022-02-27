3b2c22a1f7d50c41c0863242a27f9ad42eebd45c
Guilherme-Ferreti <guiferretis@gmail.com>
2022-02-27 15:58:27 -0300

    Indentation fix

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -3,24 +3,24 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MailService {
-  constructor (private mailerService: MailerService) {}
-  
-  async send({
-    to,
-    subject, 
-    template,
-    data
-  }: {
-    to: string;
-    subject: string;
-    template: string;
-    data: any
-  }) {
-    return this.mailerService.sendMail({
-      to,
-      subject,
-      template,
-      context: data,
-    });
-  }
+    constructor (private mailerService: MailerService) {}
+
+    async send({
+        to,
+        subject, 
+        template,
+        data
+        }: {
+        to: string;
+        subject: string;
+        template: string;
+        data: any
+        }) {
+        return this.mailerService.sendMail({
+            to,
+            subject,
+            template,
+            context: data,
+        });
+    }
 }
